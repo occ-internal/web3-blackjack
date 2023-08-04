49dfe0aacca35af0fa1eafa511c25f8afc52a782
Paola N <88536803+missmansanas@users.noreply.github.com>
2023-08-04 14:55:37 +0800

    Added comments to Blackjack.sol

--- Blackjack.sol
+++ Blackjack.sol
@@ -3,28 +3,27 @@ pragma solidity ^0.8.0;
 
 contract BlackjackGame {
     address owner;
-    uint256 minimumBetAmount = 100000000000000;
-    uint256 payoutAmnt = minimumBetAmount * 2;
+
+	// Wei amount using https://eth-converter.com/
+    uint256 minimumBetAmount = 100000000000000; // 0.0001 MATIC
+    uint256 payoutAmnt = minimumBetAmount * 2; // 0.0002 MATIC
 
     struct Game {
-        address player;
-        uint256 betAmount;
+      address player;
+      uint256 betAmount;
     }
 
     mapping(address => Game) games;
     mapping(address => uint256) balances;
 
     modifier onlyOwner() {
-        require(owner == msg.sender, "Only the owner can call this function");
-        _;
-    }
-
-    constructor() {
-        owner = msg.sender;
+      require(owner == msg.sender, "Only the owner can call this function");
+      _;
     }
 
     function placeBet() public payable {
-        require(msg.value >= minimumBetAmount, "Not enough balance for entry fee");
+        require(balances[address(this)] >= payoutAmnt, "Not enough prize money.");
+        require(msg.value == minimumBetAmount, "Place the correct bet amount.");
         uint256 amount = msg.value;
 
         balances[address(this)] += amount;
@@ -45,23 +44,23 @@ contract BlackjackGame {
     }
 
     function _transfer(address to, uint256 amount) internal virtual {
-        require(address(this) != address(0), "transfer from the zero address");
-        require(to != address(0), "transfer to the zero address");
-        unchecked { // https://solidity-by-example.org/unchecked-math/
-            balances[address(this)] = address(this).balance - amount;
-            (bool success, ) = to.call{value: amount}("");
-            require(success, "Failed to withdraw entry fee");
-            balances[to] += amount;
-        }
+      require(address(this) != address(0), "transfer from the zero address");
+      require(to != address(0), "transfer to the zero address");
+      unchecked { // https://solidity-by-example.org/unchecked-math/
+          balances[address(this)] = address(this).balance - amount;
+          (bool success, ) = to.call{value: amount}("");
+          require(success, "Failed to withdraw entry fee");
+          balances[to] += amount;
+      }
     }
 
     function balanceOf(address _address) public view returns (uint256) {
-        return balances[_address];
+      return balances[_address];
     }
 
     function withdraw() public onlyOwner {
-        (bool success, ) = msg.sender.call{value: address(this).balance}("");
-        require(success, "Failed to withdraw balance");
-        balances[address(this)] = address(this).balance;
+      (bool success, ) = msg.sender.call{value: address(this).balance}("");
+      require(success, "Failed to withdraw balance");
+      balances[address(this)] = address(this).balance;
     }
 }
